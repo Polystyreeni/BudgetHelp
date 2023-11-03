85669b6e89b50093d314a6897003219d960c30ed
Polystyreeni <jesperi.tolvanen@gmail.com>
2023-11-03 18:11:29 +0200

    Update version number

--- app/src/main/java/com/poly/budgethelp/config/VersionManager.kt
+++ app/src/main/java/com/poly/budgethelp/config/VersionManager.kt
@@ -16,7 +16,7 @@ class VersionManager {
         }
     }
     companion object {
-        var currentVersion: Version = Version(1, 0, 2)
+        var currentVersion: Version = Version(1, 0, 3)
 
         fun fetchLatestVersion(context: MainActivity) {
             if (!NetworkConfig.ALLOW_NETWORK_ACCESS)
