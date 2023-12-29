ebc50d0814f1d8297db0d43dab657d601ebef193
Polystyreeni <jesperi.tolvanen@gmail.com>
2023-12-29 18:03:33 +0200

    Update NetworkConfig

--- app/src/main/java/com/poly/budgethelp/config/NetworkConfig.kt
+++ app/src/main/java/com/poly/budgethelp/config/NetworkConfig.kt
@@ -2,10 +2,10 @@ package com.poly.budgethelp.config
 
 class NetworkConfig {
     companion object {
-        const val ALLOW_NETWORK_ACCESS = true
-        const val VERSION_URL: String = "https://drive.google.com/uc?export=download&id=1DmYsg4Gg9dYsCpBpEHakehx-t8dJKrX1"
-        const val RELEASE_URL: String = "https://github.com/Polystyreeni/BudgetHelp/releases"
-        const val MESSAGE_URL = "https://drive.google.com/uc?export=download&id=1b8Ig3Ym9w29INZ-ONLDG0t5bfuo0OGpx"
-        const val GUIDE_URL = "https://github.com/Polystyreeni/BudgetHelp/blob/main/user_manual.md"
+        const val ALLOW_NETWORK_ACCESS = false
+        const val VERSION_URL: String = "REPLACE_ME"
+        const val RELEASE_URL: String = "REPLACE_ME"
+        const val MESSAGE_URL = "REPLACE_ME"
+        const val GUIDE_URL = "REPLACE_ME"
     }
 }
